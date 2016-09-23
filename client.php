b336d00e99f4ab00035cbcf7ab50db2f994d6fa7
Baaliisu <wilangdon17@buckeyecareercenter.org>
2016-09-23 08:28:07 -0400

    Update client.php

--- client.php
+++ client.php
@@ -1,1 +1,13 @@
-
+<$php
+class client
+{
+  public function __construct()
+  {
+    $params = array('location' => 'http://location/to/server.php',
+      'uri' => 'urn://location/to/server.php',
+      'trace' => 1 /* enables tracing to debug errors, with only 1 message sent back */ );
+      
+    $this->instance = new SoapClient(NULL, $params);
+  }
+}
+$>
